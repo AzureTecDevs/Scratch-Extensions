f8723d3a6c49b59d62b2ed735183595a8f7a45a6
AzureTecDevs <150609052+AzureTecDevs@users.noreply.github.com>
2023-11-30 13:21:26 -0500

    Update math.js

--- math/math.js
+++ math/math.js
@@ -33,13 +33,12 @@ class StrExtension {
       ]
     };
   }
-
   exactlyEquals(args) {
     return args.ONE === args.TWO;
-  };
+  }
   log(args) {
     console.log(args.ONE);
   }
 }
-}
+
 Scratch.extensions.register(new StrExtension());
